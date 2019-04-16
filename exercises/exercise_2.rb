d4ce7ccc7ae1affd0966d1f0ca596f244d4be9b3
kcdporter <kiracdporter@gmail.com>
2019-04-16 20:09:51 +0000

    ex 2: storing stores as vars, and updating them - Complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+# puts "Burnaby store is called #{@store1.name}"
+@store1.update(name:"Thunderdome")
+@store2 = Store.find_by(id: 2)
+
+# puts "Burnaby store name is changed to #{@store1.name}"
+
