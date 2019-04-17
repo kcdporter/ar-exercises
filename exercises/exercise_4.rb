1e47c75e552a1219f81078c947bd5c6803a39766
kcdporter <kiracdporter@gmail.com>
2019-04-17 03:09:50 +0000

    ex 4: loading a subset - Complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Surrey = Store.create :name => "Surrey", annual_revenue: 224000, :mens_apparel => false, :womens_apparel => true
+
+Whistler = Store.create :name => "Whistler", annual_revenue: 1900000, :mens_apparel => true, :womens_apparel => false
+
+Yaletown = Store.create :name => "Yaletown", annual_revenue: 430000, :mens_apparel => true, :womens_apparel => true
+
+@mens_apparel = Store.where(mens_apparel: true)
+@womens_apparel_revenue_under_1000000 = Store.where('womens_apparel = true AND annual_revenue < 1000000')
+
+@mens_apparel.all.each do |store|
+  puts store.name
+end
+
+@womens_apparel_revenue_under_1000000.all.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
