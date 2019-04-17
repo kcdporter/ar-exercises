98aaa1ae01192c6497d6c3d67b74c6d86713b5f8
kcdporter <kiracdporter@gmail.com>
2019-04-17 03:47:02 +0000

    ex 6: One-to-many association - Complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# create_table :employees do |table|
+#   table.references :store
+#   table.column :first_name, :string
+#   table.column :last_name, :string
+#   table.column :hourly_rate, :integer
+#   table.timestamps null: false
+# end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Daphne", last_name: "Blake", hourly_rate: 60)
+@store1.employees.create(first_name: "Scooby", last_name: "Doo", hourly_rate: 60)
+@store2.employees.create(first_name: "Charley", last_name: "Schofield", hourly_rate: 60)
+@store2.employees.create(first_name: "Wendy", last_name: "Pryor", hourly_rate: 60)
+
