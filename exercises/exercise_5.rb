c46f4e80911d46ba18c7b0345afba207269e4035
kcdporter <kiracdporter@gmail.com>
2019-04-17 03:30:41 +0000

    ex 5: calculations - Complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@store_total_revenue = Store.sum("annual_revenue")
+
+@average_total_revenue = @store_total_revenue / Store.count
+
+@stores_revenue_over_1000000 = Store.where('annual_revenue > 1000000').count
+
+puts @store_total_revenue
+puts @average_total_revenue
+puts @stores_revenue_over_1000000
